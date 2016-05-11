84ee60f599acf5a50e3a309de4c64e02e47a5aa2
Scott Hill <scott289@gmail.com>
2016-05-11 09:50:03 -0700

    the redux async example works

--- app/main.js
+++ app/main.js
@@ -1,5 +1,15 @@
+import 'babel-polyfill'
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './containers/App';
+import configureStore from './store/configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+);
