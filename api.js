a3db1de5186d98231076fd6b360339f6bd8aba62
Scott Hill <scott289@gmail.com>
2016-05-29 10:09:44 -0700

    node knows const

--- api.js
+++ api.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const  router = express.Router();
+const router = express.Router();
 const pg = require('pg');
-
-
-// GET /api page.
-router.get( '/', (request, response) => {
-  response.render('api', { site_name: 'TrivNode' });
-});
-
 
 // GET /api/clues -- 10 RANDOM clues with optional limit
 router.get('/clues/:limit?', (req, res) => {
 
-  var limit = req.params.limit ? req.params.limit : 10;
+  const limit = req.params.limit ? req.params.limit : 10;
 
-  var results = [];
+  const results = [];
   pg.connect(process.env.DATABASE_URL, (err, client, done) => {
     if(err) {
       done();
@@ -22,7 +15,7 @@ router.get('/clues/:limit?', (req, res) => {
       return res.status(500).json({ success: false, data: err});
     }
     // join with category name
-    var query = client.query(
+    const query = client.query(
         "SELECT b.clue_id, a.category_name AS category, b.question, b.answer FROM clue AS b " +
         "JOIN category AS a ON a.category_id = b.category_id ORDER BY random() LIMIT " + limit + ";");
     query.on('row', (row) => {
@@ -38,14 +31,14 @@ router.get('/clues/:limit?', (req, res) => {
 
 // GET /api/clues/cat/{id} -- clues by cat id
 router.get('/clues/cat/:id', (req, res) => {
-  var results = [];
+  const results = [];
   pg.connect(process.env.DATABASE_URL, (err, client, done) => {
     if(err) {
       done();
       console.log(err);
       return res.status(500).json({ success: false, data: err});
     }
-    var query = client.query(
+    const query = client.query(
         "SELECT a.category_name AS category, b.question, b.answer FROM clue AS b " +
         "JOIN category AS a ON a.category_id = b.category_id " +
         "WHERE b.category_id = $1;", [req.params.id]);
@@ -63,14 +56,14 @@ router.get('/clues/cat/:id', (req, res) => {
 
 // GET /api/cats -- 20 RANDOM categories
 router.get('/cats', (req, res) => {
-  var results = [];
+  const results = [];
   pg.connect(process.env.DATABASE_URL, (err, client, done) => {
     if(err) {
       done();
       console.log(err);
       return res.status(500).json({ success: false, data: err});
     }
-    var query = client.query("SELECT * FROM category ORDER BY random() LIMIT 20;");
+    const query = client.query("SELECT * FROM category ORDER BY random() LIMIT 20;");
     query.on('row', (row)  =>{
       results.push(row);
     });
