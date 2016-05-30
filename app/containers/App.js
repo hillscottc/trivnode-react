1b76f3c75d57f1f4d21a79a748564595e8480bb8
Scott Hill <scott289@gmail.com>
2016-05-29 18:23:27 -0700

    refactored for clues container

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import SampleButton from '../components/SampleButton'
 import LikeButton from '../components/LikeButton'
-import ClueList from '../components/ClueList'
+import CluesContainer from './CluesContainer'
 import Header from '../components/Header'
 import styles from './App.css';
 
@@ -10,26 +9,9 @@ import styles from './App.css';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {clues: [], numToShow: 3};
+    this.state = {};
     this.buttonClick = this.buttonClick.bind(this);
-    this.changeNumToShow = this.changeNumToShow.bind(this);
-  }
 
-  updateClues(numToShow) {
-    console.log("getting clues", numToShow );
-    const _this = this;
-    this.serverRequest =
-        axios
-            .get(`/api/clues/${numToShow}`)
-            .then(function(result) {
-              _this.setState({
-                clues: result.data
-              });
-            })
-  }
-
-  componentDidMount() {
-    this.updateClues(this.state.numToShow);
   }
 
   buttonClick(value) {
@@ -37,25 +19,12 @@ export default class App extends React.Component {
     console.log("Clicked " + value);
   }
 
-  changeNumToShow(value) {
-    // this.props.dispatch(selectReddit(nextReddit));
-    console.log("Change to " + value);
-    this.setState({numToShow: parseInt(value)});
-    this.updateClues(parseInt(value));
-  }
-
-
   render() {
-    const {clues, numToShow} = this.state;
     return (
         <div className={styles.app}>
           <Header />
-          <ClueList
-                clues={clues}
-                numToShow={numToShow}
-                changeNumToShow={this.changeNumToShow}
-                numToShowOptions={["3", "5", "10", "50"]} />
-          <SampleButton value={'Click me.'} onClick={this.buttonClick}/>
+          <CluesContainer />
+          <SampleButton value={'Click me.'} onClick={this.buttonClick} />
           <LikeButton />
         </div>
     );
---
+++ app/containers/CluesContainer.js
@@ -0,0 +1,77 @@
+import React, { PropTypes } from 'react'
+import axios from 'axios';
+import { Panel } from 'react-bootstrap';
+import ClueContainer from './ClueContainer'
+import NumForm from '../components/NumForm'
+
+
+export default class CluesContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clues: [], 
+      numToShow: 3,
+      numToShowOptions: ["3", "5", "10", "50"]
+    };
+    this.changeNumToShow = this.changeNumToShow.bind(this);
+  }
+
+  updateClues(numToShow) {
+    console.log("getting clues", numToShow );
+    const _this = this;
+    this.serverRequest =
+        axios
+            .get(`/api/clues/${numToShow}`)
+            .then(function(result) {
+              _this.setState({
+                clues: result.data
+              });
+            })
+  }
+
+  componentDidMount() {
+    this.updateClues(this.state.numToShow);
+  }
+  
+
+  changeNumToShow(value) {
+    // this.props.dispatch(selectReddit(nextReddit));
+    console.log("Change to " + value);
+    this.setState({numToShow: parseInt(value)});
+    this.updateClues(parseInt(value));
+  }
+
+
+  render() {
+    const {clues, numToShow, numToShowOptions} = this.state;
+    return (
+        <Panel>
+          <div>
+            <NumForm
+                numToShow={numToShow}
+                changeNumToShow={this.changeNumToShow}
+                numToShowOptions={numToShowOptions}
+            />
+          </div>
+          <div>
+            {clues.map((clue) =>
+                <ClueContainer
+                    key={clue.clue_id}
+                    clue_id={clue.clue_id}
+                    category={clue.category}
+                    question={clue.question}
+                    answer={clue.answer}
+                />
+            )}
+          </div>
+        </Panel>
+    );
+  }
+}
+
+// CluesContainer.propTypes = {
+//   clues: PropTypes.array.isRequired,
+//   numToShow: PropTypes.number.isRequired,
+//   changeNumToShow: PropTypes.func.isRequired,
+//   numToShowOptions: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired
+// };
