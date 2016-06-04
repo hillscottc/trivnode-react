c6d8d05443b2cf230e7e3aa5151d8ebd99edf905
Scott Hill <scott289@gmail.com>
2016-06-04 10:29:40 -0700

    comments

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   output: {
-    // YOU NEED TO SET libraryTarget: 'commonjs2'
-    libraryTarget: 'commonjs2',
+    // Important.
+    libraryTarget: 'commonjs2'
   },
   module: {
     loaders: [
@@ -12,9 +12,9 @@ module.exports = {
         loaders: [
           'style-loader',
           'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]',
-          'postcss-loader',
-        ],
-      },
-    ],
-  },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
 };
