a6f7cc4cc796af58f854ffe69abe1ea7076dfc7e
Scott Hill <scott289@gmail.com>
2016-06-14 12:55:32 -0700

    in progress

--- server.js
+++ server.js
@@ -11,6 +11,61 @@ const config = require('./config');
 const app = express();
 const api = require('./api');
 
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+
+      return done(null, {username: 'me'});
+
+      // User.findOne({ username: username }, function (err, user) {
+      //   if (err) { return done(err); }
+      //   if (!user) {
+      //     return done(null, false, { message: 'Incorrect username.' });
+      //   }
+      //   if (!user.validPassword(password)) {
+      //     return done(null, false, { message: 'Incorrect password.' });
+      //   }
+      //   return done(null, user);
+      // });
+    }
+));
+
+// app.post('/login',
+//     passport.authenticate('local', { successRedirect: '/',
+//       failureRedirect: '/login' }));
+
+
+app.post('/login',
+    passport.authenticate('local'),
+    function(req, res) {
+      console.log("Login", req.user);
+      res.redirect('/' + req.user.username);
+    });
+
+
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(session({secret: '<mysecret>',
+  saveUninitialized: true,
+  resave: true}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
 // Enable api routes
 app.use('/api', api);
 
