8efc45feef784f65e84b6d247163ea7e8feea2c0
Scott Hill <scott289@gmail.com>
2016-10-22 08:06:11 -0700

    added google analytics

--- app/main.js
+++ app/main.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import ReactGA from 'react-ga';
 import App from './App.js';
 import CatsContainer from './components/cats/CatsContainer';
 import CluesContainer from './components/clues/CluesContainer';
 import CluesByCatContainer from './components/clues/CluesByCatContainer';
 import About from './components/About';
+import config from '../config'
 
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+console.log("init ga-id: ", config.ga_id);
 
+function logPageView() {
+  if (config.ga_id) {
+    ReactGA.set({page: window.location.pathname});
+    ReactGA.pageview(window.location.pathname);
+  }
+}
+
+if (config.ga_id) {
+  ReactGA.initialize(config.ga_id);
+}
 
 ReactDOM.render((
-    <Router history={browserHistory}>
+    <Router history={browserHistory} onUpdate={logPageView}>
       <Route path="/" component={App}>
 
         <IndexRoute component={CatsContainer}/>
